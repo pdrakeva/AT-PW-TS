7ce1c4ce236b3b9a953befb605677e9e831a9900
Petya Drakeva <petya.drakeva@gmail.com>
2025-03-11 22:19:32 +0200

    Lecture Nine DOM manipulations

---
+++ dom-manipulations/index.js
@@ -0,0 +1,18 @@
+var bodyEl = document.body;
+bodyEl.style.backgroundColor = "#000";
+bodyEl.style.color = "#fff";
+console.log("body background", bodyEl.style.backgroundColor);
+var divEls = document.getElementsByTagName("div");
+var btn = document.getElementsByTagName("button")[0];
+btn.style.backgroundColor = "green";
+btn.textContent = "About me";
+var headingsTwo = document.getElementsByTagName("h2");
+var paragraphs = document.getElementsByTagName("p");
+var shops = document.getElementById("shops");
+var items = document.getElementsByClassName("list-item");
+var secondItem = document.getElementsByClassName("list-item")[1];
+secondItem.textContent = "Second Element";
+var selectedH2 = document.querySelector("h2");
+selectedH2.style.color = "blue";
+var aboutUsBtn = document.querySelector(".about button");
+console.log("aboutUsBtn", aboutUsBtn);
